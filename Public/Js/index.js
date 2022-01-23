62f70c6ada199ce309cf295f5a249ec017e08e45
yasiramus <yasirayusif@gmail.com>
2022-01-23 05:03:45 -0800

    correction of carousel color

--- Public/Js/index.js
+++ Public/Js/index.js
@@ -44,7 +44,6 @@ var slideIndex = 1;
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -55,19 +54,16 @@ function showSlides(n) {
   if (slideIndex > slides.length) {
     slideIndex = 1
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000);
 }
 showSlides(slideIndex);
 
-// click dot controls to change to different you want
+// click dot controls to change to different comment section you want
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex += n);
 }
+
 
 //counter section
 const counter = document.querySelectorAll('.timer');
